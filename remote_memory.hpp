d6da3c1d015c0ea7332b521aaf137fd943d89cb4
Justas Masiulis <Justas.masiulis@yahoo.com>
2017-09-21 18:58:09 +0300

    added read/write functions

--- remote_memory.hpp
+++ remote_memory.hpp
@@ -1,6 +1,7 @@
 #ifndef REMOTE_MEMORY_HPP
 #define REMOTE_MEMORY_HPP
 
+#include "remote_memory/utils.hpp"
 #if defined(JM_PLATFORM_WINDOWS)
     #include "remote_memory/windows
 #elif defined(JM_PLATFORM_LINUX)
@@ -12,12 +13,42 @@
 namespace jm {
 
     template<class MemoryTraits>
-    class basic_remote_memory {
+    struct basic_remote_memory : public MemoryTraits {
+        using address_type = MemoryTraits::address_type;
+        using size_type = MemoryTraits::size_type;
 
-    public:
+        template <class T, class Address>
+        void read(Address address, T* buffer, size_type size) const
+        {
+            MemoryTraits::read(detail::pointer_cast<address_type>(address), buffer, size);
+        }
 
-        template <class T>
-        T read();
+        template <class T, class Address>
+        void read(Address address, T& buffer) const
+        {
+            MemoryTraits::read(detail::pointer_cast<address_type>(address), ::std::addressof(buffer), sizeof(T));
+        }
+
+        template<class T, class Address>
+        T read(Address address) const
+        {
+            typename std::aligned_storage<sizeof(T), std::alignment_of<T>::value>::type _storage;
+            MemoryTraits::read(detail::pointer_cast<address_type>(address), &_storage, sizeof(T));
+            return *static_cast<T*>(static_cast<void*>(&_storage));
+        }
+
+        template<class T, class Address>
+        void write(Address address, const T* buffer, std::size_t size) const
+        {
+            MemoryTraits::write(detail::pointer_cast<address_type>(address), buffer, size);
+        }
+
+        template<class T, class Address>
+        void write(Address address, const T& buffer) const
+        {
+            MemoryTraits::write(detail::pointer_cast<address_type>(address), std::addressof(buffer), sizeof(T));
+        }
+
     };
 
 }
---
+++ remote_memory/utils.hpp
@@ -0,0 +1,59 @@
+/*
+* Copyright 2017 Justas Masiulis
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef JM_REMOTE_MEMORY_UTILS_HPP
+#define JM_REMOTE_MEMORY_UTILS_HPP
+
+#include "config.hpp"
+
+#include <stdexcept>
+#include <limits>
+
+
+namespace spm { namespace detail {
+
+    template<std::size_t S>
+    struct uintptr_adaptive;
+
+    template<>
+    struct uintptr_adaptive<4> {
+        using type = std::uint32_t;
+    };
+    template<>
+    struct uintptr_adaptive<8> {
+        using type = std::uint64_t;
+    };
+
+    template<typename Px, typename Py>
+    constexpr Px pointer_cast(Py ptr)
+    {
+#if !defined(REMOTE_MEMORY_NO_PTR_CHECKING)
+        if (ptr > std::numeric_limits<typename uintptr_adaptive<sizeof(Px)>::type>::max())
+            throw std::overflow_error("attempt to cast to pointer of insufficient size");
+#endif
+
+        return (Px) (ptr);
+    }
+
+    template<typename Px>
+    constexpr Px pointer_cast(Px ptr) noexcept
+    {
+        ptr;
+    }
+
+}}
+
+#endif // include guard
